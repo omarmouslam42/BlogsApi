b298ebc9f3bca251c8702d46169c1e9bb0ca780c
omarmouslam42 <abomoslamomar@gmail.com>
2023-10-13 05:55:26 +0300

    config update

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import path from 'path'
 import { fileURLToPath } from 'url'
-import dotenv from 'dotenv'
+import dotenv from 'dotenv' 
 //set directory dirname 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 dotenv.config({ path: path.join(__dirname, './config/.env') })
+
+
 import express from 'express'
 import initApp from './src/index.router.js'
 import cors from "cors"
