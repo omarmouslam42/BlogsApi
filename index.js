07bfada1d3e355e38cdef5437c7da5c343070322
omarmouslam42 <abomoslamomar@gmail.com>
2023-10-16 08:50:59 +0300

    last

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ import cors from "cors"
 const port = process.env.PORT || 5000
 const app = express()
 app.use(cors())
+app.use('/', (req, res, next) => {
+    return res.send("Welcome in Blog App")
+ })
     
 initApp(app ,express)
 
