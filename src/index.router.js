e69ca86f9a62d18afa8c096a925992602bd0810a
omarmouslam42 <abomoslamomar@gmail.com>
2023-10-16 08:07:50 +0300

    welcome

--- src/index.router.js
+++ src/index.router.js
@@ -6,12 +6,14 @@ import { globalErrorHandling } from './utils/errorHandling.js'
 
 const initApp = (app, express) => {
     app.use(express.json())
- 
+    app.all('/', (req, res, next) => {
+       return res.send("Welcome in Blog App")
+    })
    app.use(`/auth`, authRouter)
    app.use(`/post`, postRouter)
    
     app.all('*', (req, res, next) => {
-        res.send("In-valid Routing Plz check url  or  method")
+        res.send("In-valid Routing Plz check url  or  method ")
     })
     app.use(globalErrorHandling)
 
--- src/modules/post/post.router.js
+++ src/modules/post/post.router.js
@@ -2,12 +2,16 @@ import { Router } from "express";
 import { fileUpload, fileValidation } from "../../utils/multer.js";
 import { auth, roles } from "../../middleware/auth.js";
 import { createPost, getPosts, getPostsById } from "./controller/post.js";
+import { validation } from "../../middleware/validation.js";
+import { addPostVal } from "./post.validation.js";
 const router = Router()
 
 
 router.route("/")
 .get(getPosts)
-.post(auth(Object.values(roles)),fileUpload(fileValidation.image).single("file"),createPost)
+.post(auth(Object.values(roles)),
+validation(addPostVal),
+fileUpload(fileValidation.image).single("file"),createPost)
 
  
 router.get("/:id",getPostsById)
